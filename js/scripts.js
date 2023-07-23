4ae97522a74263a3cd7bd41ee48d5fedfaf41476
Joseph Murray <josephwmurray@gmail.com>
2023-07-23 04:34:59 -0700

    refactor to include a prototype method, minor syntax fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
 window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
   event.preventDefault();
-  }
+  };
 }
 
 function pizza(size ,tops, cost) {
   this.size = size;
   this.tops = tops;
   this.cost = cost;
-}
+};
+
+pizza.prototype.pizzaCost = function() {
+  return parseInt(this.size) + parseInt(this.tops);
+};
 
 function myPizza() {
   const getSize = document.getElementById("size");
   const mySize = getSize.options[getSize.selectedIndex].value;
   const getTops = document.getElementsByName("topping");
   let myTops = "";
-  for (var i = 0; i < getTops.length; i++) {
+  for (let i = 0; i < getTops.length; i++) {
     if (getTops[i].checked) {
       myTops = getTops[i].value;
       break;
     }
   }
-  const myCost = parseInt(mySize) + parseInt(myTops);
-  const orderPizza = new pizza(mySize, myTops, myCost);
+  const orderPizza = new pizza(mySize, myTops, 0);
+  orderPizza.cost = orderPizza.pizzaCost();
   display(orderPizza);
 }
 
